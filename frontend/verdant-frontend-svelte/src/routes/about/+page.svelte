4a9577213918a5f65abb75e8497197ed067362ea
ian <iannpanait@gmail.com>
2025-08-29 17:02:35 -0400

    Added logos to about page

--- frontend/verdant-frontend-svelte/src/routes/about/+page.svelte
+++ frontend/verdant-frontend-svelte/src/routes/about/+page.svelte
@@ -1,3 +1,13 @@
+<script lang="ts">
+  import Figma from "../../lib/assets/tech-icons/Figma.png";
+  import Flask from "../../lib/assets/tech-icons/Flask.png.webp";
+  import MongoDB from "../../lib/assets/tech-icons/MongoDB.png";
+  import Python from "../../lib/assets/tech-icons/Python.png";
+  import Scrapy from "../../lib/assets/tech-icons/Scrapy.png";
+  import Svelte from "../../lib/assets/tech-icons/Svelte.png";
+  import Typescript from "../../lib/assets/tech-icons/Typescript.png";
+</script>
+
 <div id="homepage-about">
   <div id="about-main">
     <div id="about-us-header-div"><h1>About Verdant</h1></div>
@@ -18,14 +28,20 @@
       >
         <h1 style="font-size: 36px; margin-bottom: 5px;">Frontend</h1>
         <div style="display: flex; gap: 25px;">
-          <div class="tech-bubble"></div>
-          <div class="tech-bubble"></div>
+          <div class="tech-bubble">
+            <img src={Figma} style="height: 80px;" />
+          </div>
+          <div class="tech-bubble">
+            <img src={Svelte} style="height: 80px;" />
+          </div>
         </div>
         <div
           id="third-frontend-bubble"
           class="tech-bubble"
           style="margin-top: 20px;"
-        ></div>
+        >
+          <img src={Typescript} style="height: 80px;" />
+        </div>
       </div>
       <div class="tech-stack-divider"></div>
 
@@ -35,8 +51,12 @@
       >
         <h1 style="font-size: 36px; margin-bottom: 5px;">Backend</h1>
         <div class="tech-bubble-wrapper">
-          <div class="tech-bubble"></div>
-          <div class="tech-bubble" style="margin-top: 20px;"></div>
+          <div class="tech-bubble">
+            <img src={MongoDB} style="height: 80px;" />
+          </div>
+          <div class="tech-bubble" style="margin-top: 20px;">
+            <img src={Flask} style="height: 80px;" />
+          </div>
         </div>
       </div>
 
@@ -48,8 +68,12 @@
       >
         <h1 style="font-size: 36px; margin-bottom: 5px;">Crawler</h1>
         <div class="tech-bubble-wrapper">
-          <div class="tech-bubble"></div>
-          <div class="tech-bubble" style="margin-top: 20px;"></div>
+          <div class="tech-bubble">
+            <img src={Scrapy} style="height: 80px;" />
+          </div>
+          <div class="tech-bubble" style="margin-top: 20px;">
+            <img src={Python} style="height: 80px;" />
+          </div>
         </div>
       </div>
     </div>
